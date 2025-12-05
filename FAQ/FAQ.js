f02a4c866c8169d6e57e90c777c72150391ca871
marouanepro274-design <marouanepro274@gmail.com>
2025-12-05 11:51:36 +0100

    Update FAQ.js

--- FAQ/FAQ.js
+++ FAQ/FAQ.js
@@ -1,1 +1,186 @@
 
+
+sizebox1 = document.getElementById("a1");
+
+Texe1 = document.getElementById("T1");
+
+
+sizebox2 = document.getElementById("a2");
+
+Texe2 = document.getElementById("T2");
+
+
+sizebox3 = document.getElementById("a3");
+
+Texe3 = document.getElementById("T3");
+
+
+sizebox4 = document.getElementById("a4");
+
+Texe4 = document.getElementById("T4");
+
+
+sizebox5 = document.getElementById("a5");
+
+Texe5 = document.getElementById("T5");
+
+
+
+
+sizebox1.addEventListener("click", function(){
+
+
+sizebox1.style.height = "100px";
+
+
+Texe1.textContent = "Nous proposons des services de création d'une identité digital  "
+
+Texe1.style.fontSize = "15px"
+
+Texe1.style.color = "white"
+
+
+Texe1.style.opacity = "70%"
+
+
+
+
+
+});
+
+
+sizebox1.addEventListener("mouseout", function(){
+
+sizebox1.style.height = "50px";
+
+Texe1.textContent = ""
+
+
+
+});
+
+
+sizebox2.addEventListener("click", function(){
+
+
+sizebox2.style.height = "100px";
+
+
+Texe2.textContent = "La période de création elle varie selon vos besoins et aussi selon le plan choisi"
+
+Texe2.style.fontSize = "15px"
+
+Texe2.style.color = "white"
+
+Texe2.style.opacity = "70%"
+
+
+
+
+});
+
+
+sizebox2.addEventListener("mouseout", function(){
+
+sizebox2.style.height = "50px";
+
+Texe2.textContent = ""
+
+
+
+});
+
+
+sizebox3.addEventListener("click", function(){
+
+
+sizebox3.style.height = "100px";
+
+
+Texe3.textContent = "Oui, vérifiez la page des tarifs pour plus des détails"
+
+Texe3.style.fontSize = "15px"
+
+Texe3.style.color = "white"
+
+Texe3.style.opacity = "70%"
+
+
+
+
+});
+
+
+sizebox3.addEventListener("mouseout", function(){
+
+sizebox3.style.height = "50px";
+
+Texe3.textContent = ""
+
+
+
+});
+
+
+sizebox4.addEventListener("click", function(){
+
+
+sizebox4.style.height = "100px";
+
+
+Texe4.textContent = "Yes, notre mission, c'est de créer une identité digitale professionnelle pour votre entreprisee"
+
+Texe4.style.fontSize = "15px"
+
+Texe4.style.color = "white"
+
+Texe4.style.opacity = "70%"
+
+
+
+
+});
+
+
+sizebox4.addEventListener("mouseout", function(){
+
+sizebox4.style.height = "50px";
+
+Texe4.textContent = ""
+
+
+
+});
+
+
+
+
+sizebox5.addEventListener("click", function(){
+
+
+sizebox5.style.height = "100px";
+
+
+Texe5.textContent = "Nous avons deux plans chacun avec ses spécifications"
+
+Texe5.style.fontSize = "15px"
+
+Texe5.style.color = "white"
+
+Texe5.style.opacity = "70%"
+
+
+
+
+});
+
+
+sizebox5.addEventListener("mouseout", function(){
+
+sizebox5.style.height = "50px";
+
+Texe5.textContent = ""
+
+
+
+});
